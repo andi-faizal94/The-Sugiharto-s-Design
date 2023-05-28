de72c7b10c4118a3c65973a777d695a43d597d60
Andi Faizal <andifaizal647@gmail.com>
2023-05-28 07:15:17 +0700

    change port default in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+
   plugins: [
     react({
       include: '**/*.tsx',
